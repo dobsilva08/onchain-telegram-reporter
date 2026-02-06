94675824227aaf960cfbe4be10ca214f0422e01d
dobsilva08 <119372769+dobsilva08@users.noreply.github.com>
2026-02-06 19:52:45 -0300

    Create telegram_utils.py

---
+++ telegram_utils.py
@@ -0,0 +1,14 @@
+import requests
+import os
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+def send_message(text):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": text,
+        "parse_mode": "Markdown"
+    }
+    requests.post(url, json=payload)
