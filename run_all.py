76630334baef283e1f1ae707835d5ea9502cdc76
dobsilva08 <119372769+dobsilva08@users.noreply.github.com>
2026-02-06 21:28:57 -0300

    Update run_all.py

--- run_all.py
+++ run_all.py
@@ -1,10 +1,13 @@
-from report_market import generate_market_report
-from report_onchain import generate_onchain_report
 from telegram_sender import send
+from market_report import generate_market_report
 
 def main():
-    send(generate_market_report())
-    send(generate_onchain_report())
+    message = generate_market_report()
+
+    if not message or not isinstance(message, str):
+        raise RuntimeError("Relatório vazio ou inválido — nada para enviar ao Telegram")
+
+    send(message)
 
 if __name__ == "__main__":
     main()
