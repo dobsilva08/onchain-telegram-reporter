0f0e96fb19fd4c2240e3fbf9ce805a3b4f99b75c
dobsilva08 <119372769+dobsilva08@users.noreply.github.com>
2026-02-06 21:43:35 -0300

    Update telegram_sender.py

--- telegram_sender.py
+++ telegram_sender.py
@@ -1,12 +1,14 @@
 import os
 import requests
 
+
 def send(message: str):
     token = os.getenv("TELEGRAM_BOT_TOKEN")
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
 
     if not token:
         raise RuntimeError("TELEGRAM_BOT_TOKEN não definido nos secrets do GitHub")
+
     if not chat_id:
         raise RuntimeError("TELEGRAM_CHAT_ID não definido nos secrets do GitHub")
 
@@ -15,8 +17,12 @@ def send(message: str):
     payload = {
         "chat_id": chat_id,
         "text": message,
-        "parse_mode": "HTML"
+        "parse_mode": "Markdown"
     }
 
-    response = requests.post(url, json=payload, timeout=10)
-    response.raise_for_status()
+    response = requests.post(url, json=payload, timeout=30)
+
+    if response.status_code != 200:
+        raise RuntimeError(
+            f"Erro ao enviar mensagem Telegram: {response.status_code} - {response.text}"
+        )
